ded045c9560daa2ac3dec92a917b6402cf18491c
artem247 <artem.247@gmail.com>
2020-07-23 17:46:13 +0300

    require refactoring

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
 require 'yaml'
+
 require_relative 'codebreaker/helpers/defaults'
 require_relative 'codebreaker/helpers/validators'
+
 require_relative 'codebreaker/services/code_generator'
 require_relative 'codebreaker/services/guess_parser'
 require_relative 'codebreaker/services/statistics'
+
 require_relative 'codebreaker/errors/class_error'
 require_relative 'codebreaker/errors/guess_error'
 require_relative 'codebreaker/errors/occurence_error'
 require_relative 'codebreaker/errors/string_size_error'
+
 require_relative 'codebreaker/models/difficulty'
 require_relative 'codebreaker/models/game'
 require_relative 'codebreaker/models/user'
+
 require_relative 'codebreaker/version'
