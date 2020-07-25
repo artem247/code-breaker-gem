598fddbcf36c6fe08b3d9165a18c1f9fd2d84f07
artem247 <artem.247@gmail.com>
2020-07-25 12:49:47 +0300

    namespaces fix

--- lib/codebreaker/helpers/defaults.rb
+++ lib/codebreaker/helpers/defaults.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-module Defaults
-  USER_NAME_SIZE = [3, 20].freeze
-  CODE_SIZE = 4
-  RESULT_DEFAULT = { exact_match: 0, number_match: 0, no_match: 0 }.freeze
-  CODE_RANGE = (1..6).freeze
-  CODE_PATTERN =
+module Codebreaker
+  module Defaults
+    USER_NAME_SIZE = [3, 20].freeze
+    CODE_SIZE = 4
+    RESULT_DEFAULT = { exact_match: 0, number_match: 0, no_match: 0 }.freeze
+    CODE_RANGE = (1..6).freeze
+    CODE_PATTERN = /^[1-6]{4}$/.freeze
     DEFAULT_DIFFICULTIES = { easy: { attempts: 15, hints: 2 },
                              hard: { attempts: 10, hints: 1 },
                              hell: { attempts: 5, hints: 1 } }.freeze
+  end
 end
